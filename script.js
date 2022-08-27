323ff437e0e2ff7588b7b07186040318cf9704c0
Bikal Thapa <88891805+bikalthapa@users.noreply.github.com>
2022-08-27 11:07:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function circle1(){
+    const audio = new Audio();
+    audio.src="one.mp3";
+    audio.play();
+}
+
+function circle2(){
+    const audio2 = new Audio();
+    audio2.src="one.mp3";
+    audio2.play();
+}
